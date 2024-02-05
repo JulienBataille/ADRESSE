8735e7b92b467c41bf72fd102d632d52e05ddb5e
Landesprit <julien.bataille40@gmail.com>
2024-02-05 15:24:21 +0100

    MAJ

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
-let nom = document.getElementById("nom").textContent;
-let prenom = document.getElementById("prenom").textContent;
-let anniv = document.getElementById("birth");
-let ville = document.getElementById("ville").textContent;
-let email = document.getElementById("email").textContent;
-let password = document.getElementById("password").textContent;
+document.addEventListener("DOMContentLoaded", function(){
+    let form = document.querySelector("form");
 
+    form.addEventListener("submit", function(event){
+        let nom = document.getElementById("nom").value;
+        let prenom = document.getElementById("prenom").value;
+        let birthday = document.getElementById("birth").value;
+        let ville = document.getElementById("ville").value;
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
 
+    //regex pour les emails gmail.com
+        let emailRegex = /@gmail.com$/;
+        let nomRegex = /^[A-Za-z]{2,255}$/;
+        let prenomRegex = /^[A-Za-z-'\s]{2,255}$/;
+        let villeRegex = /^[A-Za-z-'\s]{2,255}$/;
+        let passwordRegex =/^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{14,}$/;
 
-const validationButton = document.getElementById("bouton");
+    // utilsiateur est agé de plus de 13ans
+    let date = new Date();
+    let userYear = new Date(birthday);
 
-function verification() {
-    let birthday = new Date(anniv.value);
-    let today = new Date();
-    if (birthday <= today.setFullYear(today.getFullYear()-18)){
-        alert("vous etes majeur")
-    } else {
-        alert("vous n'etes pas majeur")
+    let age = date.getFullYear - userYear.getFullYear;
+
+    if (age < 13){
+        alert("Vous devez avoir plus de 13 ans pour vous inscrire");
+        event.preventDefault()
+        return
     }
-}
+
+
+
+    })
+})
